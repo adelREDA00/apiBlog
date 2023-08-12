332a17ad55f993c27ac1beea1e155c4cc8c5ea02
adel <toumireda34@gmail.com>
2023-08-12 17:04:31 +0100

    12A

--- index.js
+++ index.js
@@ -47,12 +47,12 @@ mongoose.connect(process.env.MONGO_URL,
 
 //--------midlewares--------//
 // Enable CORS for all routes
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
-});
+});*/
 //cookiepar
 app.use(cookieParser())
 //creating auth users
